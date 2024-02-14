759a13583aa5931889f5cac5b11eda83bc59d9b0
Ahmad Rishi <ahmadrishi@gmail.com>
2024-02-14 13:04:16 +0300

    Dynamic URLs

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/')
@@ -23,6 +23,6 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login Requested for {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
