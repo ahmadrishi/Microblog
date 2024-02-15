9061dab02c06dc5069cc2b586b75d2603a82942c
Ahmad Rishi <ahmadrishi@gmail.com>
2024-02-15 17:10:24 +0300

    Authentication

--- app/models.py
+++ app/models.py
@@ -2,9 +2,12 @@ from typing import Optional
 from datetime import datetime, timezone
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
@@ -17,7 +20,12 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
     
-
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
@@ -30,3 +38,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
+    
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
